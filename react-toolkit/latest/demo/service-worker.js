94387394c5cb5ae99bed498683110203fef72c88
Guillaume Chervet <guillaume.chervet@axa.fr>
2019-02-14 16:38:54 +0000

    doc(toolkit) publish website 1.1.1

--- react-toolkit/latest/demo/service-worker.js
+++ react-toolkit/latest/demo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.75c473d1e208b43f0bef1ed796486313.js"
+  "./precache-manifest.ea15a367b3a1b55c4fefeef7018f99f9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
