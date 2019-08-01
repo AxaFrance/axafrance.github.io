57dfc5cb244804da50d14b2d8df89c541e50a60d
Guillaume Chervet <guillaume.chervet@axa.fr>
2019-08-01 09:48:25 +0000

    fix manually desync of version

---
+++ react-toolkit/latest/storybook/main.e7a40e625013d484dc27.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{451:function(n,o,p){p(452),p(555),n.exports=p(987)},555:function(n,o,p){"use strict";p.r(o);p(556),p(754),p(950)}},[[451,1,2]]]);
