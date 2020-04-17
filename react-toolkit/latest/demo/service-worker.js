4cf36f9852f1fce84302f77d590f71c8e4c71d15
Build-CI <build-ci@axa.fr>
2020-04-17 09:24:34 +0000

    doc(toolkit) publish website 1.3.8-alpha.0

--- react-toolkit/latest/demo/service-worker.js
+++ react-toolkit/latest/demo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.545c13d3c91ad0caa4f7e324a4470c23.js"
+  "./precache-manifest.349c5bf190084e530d1ee396d04b721c.js"
 );
 
 self.addEventListener('message', (event) => {
