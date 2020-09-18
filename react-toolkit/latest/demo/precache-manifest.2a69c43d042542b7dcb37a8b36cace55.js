65363a3e06f5c9ccb2bca9aff2a9c1ca72f39fc2
Build-CI <build-ci@axa.fr>
2020-09-18 10:18:04 +0000

    doc(toolkit) publish website 1.3.21

---
+++ react-toolkit/latest/demo/precache-manifest.2a69c43d042542b7dcb37a8b36cace55.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f342554731a69a794e2466a0ddc7df8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f461e2dcd7e19f940e11",
+    "url": "./static/css/2.c9ec60c8.chunk.css"
+  },
+  {
+    "revision": "e446afec0c110d64d296",
+    "url": "./static/css/main.c1b1ab13.chunk.css"
+  },
+  {
+    "revision": "f461e2dcd7e19f940e11",
+    "url": "./static/js/2.db7da990.chunk.js"
+  },
+  {
+    "revision": "396df9475395f6611684e24ae9820953",
+    "url": "./static/js/2.db7da990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e446afec0c110d64d296",
+    "url": "./static/js/main.f1748831.chunk.js"
+  },
+  {
+    "revision": "43e0eee0131326e2abfa",
+    "url": "./static/js/runtime-main.0a647f7d.js"
+  },
+  {
+    "revision": "64d7776b6edca34739c040d19a9f78dc",
+    "url": "./static/media/icons-sprite.64d7776b.svg"
+  },
+  {
+    "revision": "35e5571c5a487ebf92b27bec64468129",
+    "url": "./static/media/icons.35e5571c.svg"
+  },
+  {
+    "revision": "3df2e0194bb91ee5d9c0efa007405f51",
+    "url": "./static/media/icons.3df2e019.eot"
+  },
+  {
+    "revision": "4afdda3d648854d0dd02befa11ec538f",
+    "url": "./static/media/icons.4afdda3d.woff"
+  },
+  {
+    "revision": "d5d136433059243739a08816139c7eed",
+    "url": "./static/media/icons.d5d13643.ttf"
+  },
+  {
+    "revision": "d4ae4eac88e337a3e94f28b9ee5d189c",
+    "url": "./static/media/logo-axa.d4ae4eac.svg"
+  }
+]);
--- react-toolkit/latest/demo/service-worker.js
+++ react-toolkit/latest/demo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.73239a1e4bc98f7f9a59f55c85092df7.js"
+  "./precache-manifest.2a69c43d042542b7dcb37a8b36cace55.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
