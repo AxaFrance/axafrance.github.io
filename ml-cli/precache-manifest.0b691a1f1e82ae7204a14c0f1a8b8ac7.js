c6fc73a531311dbaeef422607d5591cf7b857398
Lilian Delouvy <lilian.delouvy@axa.fr>
2021-11-26 14:41:28 +0100

    fix(Demo): add required files to storybook

---
+++ ml-cli/precache-manifest.0b691a1f1e82ae7204a14c0f1a8b8ac7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e4502bc04872a67f43",
+    "url": "0.13c54676.iframe.bundle.js"
+  },
+  {
+    "revision": "4c9003b271e6b79f5e2d",
+    "url": "1.060378f7.iframe.bundle.js"
+  },
+  {
+    "revision": "98da805edaf1b22bf03c",
+    "url": "5.cf9a7553.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "5.cf9a7553.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdef5ab19e5725169037",
+    "url": "6.1e2981e4.iframe.bundle.js"
+  },
+  {
+    "revision": "399a936b3c1cae1cd7cf",
+    "url": "7.5256cab3.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "7.5256cab3.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa078a0f9dcddd0f31b",
+    "url": "8.43bf70a1.iframe.bundle.js"
+  },
+  {
+    "revision": "b1fd9f3b196dfc53d222",
+    "url": "9.88ad8f84.iframe.bundle.js"
+  },
+  {
+    "revision": "ed9329944c796e858b9a0641f2a5d838",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "316da5fce11473f78c49",
+    "url": "main.07dfcfa2.iframe.bundle.js"
+  },
+  {
+    "revision": "5091dde79e5c9528b1f3",
+    "url": "runtime~main.6503c6c7.iframe.bundle.js"
+  },
+  {
+    "revision": "316da5fce11473f78c49",
+    "url": "static/css/main.2223adcd.chunk.css"
+  },
+  {
+    "revision": "f604c8febf414f2cc68f",
+    "url": "static/css/vendors~main.b4d2499d.chunk.css"
+  },
+  {
+    "revision": "35e5571c5a487ebf92b27bec64468129",
+    "url": "static/media/icons.35e5571c.svg"
+  },
+  {
+    "revision": "3df2e0194bb91ee5d9c0efa007405f51",
+    "url": "static/media/icons.3df2e019.eot"
+  },
+  {
+    "revision": "4afdda3d648854d0dd02befa11ec538f",
+    "url": "static/media/icons.4afdda3d.woff"
+  },
+  {
+    "revision": "d5d136433059243739a08816139c7eed",
+    "url": "static/media/icons.d5d13643.ttf"
+  },
+  {
+    "revision": "d4ae4eac88e337a3e94f28b9ee5d189c",
+    "url": "static/media/logo-axa.d4ae4eac.svg"
+  },
+  {
+    "revision": "fcc94e3f33d422e85648898d55eff572",
+    "url": "static/media/sample_image.fcc94e3f.png"
+  },
+  {
+    "revision": "f604c8febf414f2cc68f",
+    "url": "vendors~main.33aaaae8.iframe.bundle.js"
+  },
+  {
+    "revision": "172e354cf608f8d17c005428005088a7",
+    "url": "vendors~main.33aaaae8.iframe.bundle.js.LICENSE.txt"
+  }
+]);
